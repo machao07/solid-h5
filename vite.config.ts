162cb1f218667a2e4595a2e237b80fb2d7a68ab7
machao07 <machao_07@163.com>
2023-12-06 10:57:58 +0800

    配置代理

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,14 @@ import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 
 export default defineConfig({
-  plugins: [solid()],
+    plugins: [solid()],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'xxx',
+                changeOrigin: true,
+            }
+        },
+        host: '0.0.0.0'
+    },
 })
