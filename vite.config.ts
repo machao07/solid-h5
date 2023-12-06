619f902d92111eb7a75544c59d5de4ea5181b6ab
machao07 <machao_07@163.com>
2023-12-06 11:01:36 +0800

    添加环境变量

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import solid from 'vite-plugin-solid'
 
-export default defineConfig({
-    plugins: [solid()],
-    server: {
-        proxy: {
-            '/api': {
-                target: 'xxx',
-                changeOrigin: true,
-            }
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd())
+    return {
+        plugins: [solid()],
+        base: env.VITE_APP_ROUTER_PREFIX,
+        server: {
+            proxy: {
+                '/api': {
+                    target: 'xxx',
+                    changeOrigin: true,
+                }
+            },
+            host: '0.0.0.0'
         },
-        host: '0.0.0.0'
-    },
+    }
 })
