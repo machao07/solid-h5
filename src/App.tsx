96c9b22a334b26cd897168c0c790e0dbc92b929e
machao07 <machao_07@163.com>
2023-12-07 10:25:45 +0800

    项目根文件修改

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import type { Component } from 'solid-js';
 import { Router, Route, Routes, hashIntegration } from "@solidjs/router";
 import { lazy } from "solid-js";
 const PlanOne = lazy(() => import('./pages/PlanOne'));
+const PlanTwo = lazy(() => import('./pages/PlanTwo'));
 
 const App: Component = () => {
     return (
         <>
-            <div class='toast-info' />
+            <div class='toast-info'></div>
             <Router source={hashIntegration()}>
                 <Routes>
-                    <Route path="/" component={PlanOne} />
+                    <Route path="/" component={PlanTwo} />
                     <Route path="/planOne" component={PlanOne} />
+                    <Route path="/planTwo" component={PlanTwo} />
                 </Routes>
             </Router>
         </>
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,23 @@
 /* @refresh reload */
-import { render } from 'solid-js/web'
+import { render } from 'solid-js/web';
 import { Router } from "@solidjs/router";
 import './index.css';
 import './style/reset.css';
-import App from './App'
+import App from './App';
 
 import 'amfe-flexible/index.js';
 
-const root = document.getElementById('root')
+const root = document.getElementById('root');
 
-render(() => (
-    <Router>
-        <App />
-    </Router>
-), root!)
+if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
+    throw new Error(
+        'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
+    );
+}
+
+render(
+    () => (
+        <Router>
+            <App />
+        </Router>
+    ), root!);
